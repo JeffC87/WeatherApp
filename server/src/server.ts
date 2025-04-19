cf9c5048f684b9f0484fa5e6fa9633d6fbc9b54f
Jeff <jlc68@me.com>
2025-04-19 18:50:38 -0500

    updated server paths

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,15 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
